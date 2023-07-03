62e3e4717ff6e70e992b1fba6acc9a540f2a16a0
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-07-03 11:03:22 +0200

    Projet initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,55 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1000px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1200px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '0.25rem',
+          sm: '2rem',
+          lg: '0rem',
+          xl: '0rem',
+          '2xl': '0rem',
+        },
+      },
+      colors: {
+        'app-blue': '#1D3A8A',
+        'app-yellow': '#FDDE6D',
+        'app-light-blue': '#EEF5FA',
+        'app-light-gray': '#F8FAFB'
+
+      },
+      gridTemplateRows: {
+        // Simple 8 row grid
+        '5': 'repeat(8, minmax(0, 1fr))',
+        '8': 'repeat(8, minmax(0, 1fr))',
+        '9': 'repeat(9, minmax(0, 1fr))',
+        '10': 'repeat(10, minmax(0, 1fr))',
+
+        // Complex site-specific row configuration
+        'layout': '200px minmax(900px, 1fr) 100px',
+      }
     },
   },
-  plugins: [],
 }
